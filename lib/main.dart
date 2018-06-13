d2d3eb4e7012ab3766b6426eafe92a9d6bd32109
Lemmah <lemayiannakolah@gmail.com>
2018-06-14 00:49:30 +0300

    feat(saved-favorites): add route to view saved favorites
    - add action widget to AppBar Widget with an icon
    - define an onPressed behavior that is _pushSaved
    - implement _pushSaved to use MaterialPageRoute and it's builder
    
    [Finishes Step 6]

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,9 @@ class RandomWordsState extends State<RandomWords> {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
@@ -62,9 +65,40 @@ class RandomWordsState extends State<RandomWords> {
             _saved.remove(pair);
           } else {
             _saved.add(pair);
-          }    
+          }
         });
       },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile
+              .divideTiles(
+                context: context,
+                tiles: tiles,
+              )
+              .toList();
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          );
+        },
+      ),
     );
   }
 }
